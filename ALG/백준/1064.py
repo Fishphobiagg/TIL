3c79fea33147c6f62062efc8b1fbf3f9d94f051a
Fishphobiagg <2017100911@khu.ac.kr>
2023-01-13 16:02:31 +0900

    백준1064번 풀이

--- ALG/백준/1064.py
+++ ALG/백준/1064.py
@@ -1,15 +1,14 @@
-import math
 x_a, y_a, x_b, y_b, x_c, y_c = map(int, input().split())
 
-a = math.sqrt((x_a - x_b)**2 + (y_a - y_b)**2)
-b = math.sqrt((x_b - x_c)**2 + (y_b - y_c)**2)
-c = math.sqrt((x_c - x_a)**2 + (y_c - y_a)**2)
+if (x_a-x_b)*(y_a-y_c) == (y_a-y_b)*(x_a-x_c):
+    print(-1.0)
+    exit(0)
 
-if a == b:
-    
-elif b == c:
-else:
+a = ((x_a - x_b)**2 + (y_a - y_b)**2)**0.5
+b = ((x_b - x_c)**2 + (y_b - y_c)**2)**0.5
+c = ((x_c - x_a)**2 + (y_c - y_a)**2)**0.5
 
-A = [a, b, c]
+length = [a+c, a+b, c+b]
+result = max(length) - min(length)
 
-print(A)
+print(2*result)
