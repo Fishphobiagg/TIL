6df97ee277cca7c395175dcf25389d58286e72d2
Fishphobiagg <2017100911@khu.ac.kr>
2023-01-13 17:06:09 +0900

    백준1094 풀이 미완성

---
+++ ALG/백준/1094.py
@@ -0,0 +1,14 @@
+#2진법
+X = int(input())
+count = 0
+stick = [32, 16, 8, 4, 2, 1]
+
+if X == 64:
+    print(1)
+    exit(0)
+else:
+    for i in stick:
+        if X > i:
+            X -= i
+            count += 1
+        else:
