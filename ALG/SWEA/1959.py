258dcb5babefb5aafd3c3acca99f99db8055de15
Fishphobiagg <2017100911@khu.ac.kr>
2023-01-13 11:29:25 +0900

    1959 수정

--- ALG/SWEA/1959.py
+++ ALG/SWEA/1959.py
@@ -2,9 +2,33 @@ T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    A, B = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
     result = []
     if len(A) > len(B):
-        for i in range(len(A)-len(B)):
-            result = list(map(''))
-a = 1
+        c = len(A)-len(B)
+        for __ in range(c):
+            B.append(0)
+        for i in range(0, c):
+            sum = 0
+            for a,b in zip(A, B):
+                sum += a*b
+            result.append(sum)
+            B[i] = B[i-1]
+    elif len(B) > len(A):
+        c = len(B)-len(A)
+        for __ in range(c):
+            A.append(0)
+        for i in range(0, c):
+            sum = 0
+            for a,b in zip(A, B):
+                sum += a*b
+            result.append(sum)
+            A[c] = A[c+i]
+    else:
+        sum = 0
+        for a,b in A, B:
+            sum += a*b
+        result.append(sum)
+    print(result)
+    print(f'#{_+1} {max(result)}')
