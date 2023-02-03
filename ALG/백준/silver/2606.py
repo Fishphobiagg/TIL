c09f64983edae7c6cb52ac3696cc946020b0ea98
Fishphobiagg <2017100911@khu.ac.kr>
2023-02-03 16:01:58 +0900

    백준 2606번 풀이

--- ALG/백준/silver/2606.py
+++ ALG/백준/silver/2606.py
@@ -1,10 +1,7 @@
 N = int(input())
 connection = int(input())
-infection = [[0*N]*N]
+infection = [[]*N]
 
-for i in range(connection):
-    A, B = map(int, input().split())
-    infection[A][B] = 1
-    infection[B][A] = 1
+# 2차원 배열에 저장
 
 print(infection)
