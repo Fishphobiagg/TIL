56d2e0507b47082893e7a8b088d0dc89a9d1e338
Fishphobiagg <dlauddn6211@icloud.com>
2023-02-14 00:58:44 +0900

    1966.py

--- ALG/백준/silver/1966.py
+++ ALG/백준/silver/1966.py
@@ -1,8 +1,28 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 T = int(input())
-
 for i in range(T):
     N, M = map(int, input().split())
     importance = deque(list(map(int, input().split())))
+<<<<<<< Updated upstream
     target_idx = M
     
+=======
+    count = 0
+    while True:
+        max_i = max(importance)
+        c = importance.popleft()
+        if M == -1:
+            M = len(importance)
+        if c == max_i and M == 0:
+            count += 1
+            print(count)
+            break
+        elif c == max_i:
+            count += 1
+            M -= 1
+        else:
+            importance.append(c)
+            M -= 1
+>>>>>>> Stashed changes
