afe5cdbf67545e4dd09f8191f2f1c28445b2c3c7
Fishphobiagg <dlauddndlek@naver.com>
2023-03-23 12:29:35 +0900

    2110번 풀이

--- ALG/백준/gold/2110.py
+++ ALG/백준/gold/2110.py
@@ -2,26 +2,28 @@
 import sys
 input = sys.stdin.readline
 
+'''
+설치 기준 거리보다 전에 설치했던 집과의 거리가 클 경우 설치
+'''
 N, C = map(int, input().split())
 home = [int(input()) for _ in range(N)]
 home.sort()
-result = 0
 start, end = 1, home[-1] - home[0]
+result = 0
 if C == 2:
-    print(home[1]-home[0])
-    exit()
-
-while start < end:
-    now = (start+end)//2
-    cnt = 1
-    last = 0
-    for i in range(1, N):
-        if home[i]-home[last] >= now:
-            cnt += 1
-            last = i
-    if cnt >= C:
-        result = now
-        start = now+ 1
-    elif cnt < C:
-        end = now
-print(result)
+    print(home[-1]-home[0])
+else:
+    while start < end:
+        mid = (start+end)//2 # 집 설치 간격
+        last = home[0] # 전에 설치했던 집
+        cnt = 1 #설치 개수를 저장하기 위한 변수
+        for i in range(1, N):
+            if mid <= home[i]-last:
+                cnt += 1
+                last = home[i]
+        if cnt >= C: # 설치를 너무 많이 한 경우 - 간격 늘리기
+            start = mid+1
+            result = mid
+        elif cnt < C: # 너무 적게 설치한 경우 - 간격 좁히기
+            end = mid
+    print(result)
