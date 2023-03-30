a3a0be620d71569ff545d80c89907252f3548089
fishphobiagg <dlauddn6211@icloud.com>
2023-03-31 01:37:11 +0900

    17070

--- ALG/백준/gold/17070.py
+++ ALG/백준/gold/17070.py
@@ -1,38 +1,64 @@
-# 파이크 옮기기 1
-from collections import deque
+# 파이크 옮기기 1 dfs
 
 '''
 시작 지점부터 끝 지점까지의 좌표 사이에 있는 칸은 모두 파이프
 전 end는 다음 start가 됨
 45도만 꺾기 가능
+
+벽 부딪히는 경우의 수 제외하면 DP로 해결 가능
 '''
 pipe = [list(map(int,input().split())) for _ in range(int(input()))]
-dp = [[0]*len(pipe) for _ in range(len(pipe))]
-
-start, end = (0,0), (0,1)
-dp[0][0] = 1
-direction = 1
-move = [(1,1), (0,1), (1,0)]
+N = len(pipe)
+end = (0,1)
+direction = 1 # 1 = 가로 0 = 대각 2 = 세로
 cnt = 0
-def pipe_dp(s, e, d):
+def dfs(e,d):
     global cnt
-    x_1, y_1 = s
-    x_2, y_2 = e
-    if x_1 == len(pipe) or y_1 == len(pipe) or x_2 == len(pipe) or y_2 == len(pipe):
-        return
-    for i in range(x_1, x_2+1):
-        for j in range(y_1, y_2+1):
-            if pipe[i][j]: # 벽이 있을 경우
-                return
-    if (x_2, y_2) == (len(pipe)-1, len(pipe)-1):
-        cnt +=1
-        return
-    # dp[x_2][y_2] += dp[x_1][y_1]
-    for i in range(3): # 3방향 고고
-        if (d==1 and i==2) or (d==2 and i==1):
-            continue
-        pipe_dp(e, (e[0]+move[i][0], e[1]+move[i][1]), i)
+    if d == 0:
+        if e[0]+1<N and e[1]+1<N:
+            if not pipe[e[0]][e[1]]+pipe[e[0]+1][e[1]]+pipe[e[0]][e[1]+1]+pipe[e[0]+1][e[1]+1]:
+                if (e[0]+1, e[1]+1) == (N-1, N-1):
+                    cnt +=1
+                elif e[0]+1<N and e[1]+1<N:
+                    dfs((e[0]+1, e[1]+1), 0)
+        if e[1]+1 < N:
+            if not pipe[e[0]][e[1]] + pipe[e[0]][e[1]+1]:
+                if (e[0], e[1]+1) == (N-1, N-1):
+                    cnt +=1
+                elif e[1]+1 < N-1:
+                    dfs((e[0], e[1]+1), 1)
+        if e[0]+1 < N:
+            if not pipe[e[0]][e[1]] + pipe[e[0]+1][e[1]]:
+                if (e[0]+1, e[1]) == (N-1, N-1):
+                    cnt +=1
+                elif e[0]+1 < N-1:
+                    dfs((e[0]+1, e[1]), 2)
+    elif d == 1:
+        if e[0]+1 < N and e[1]+1<N:
+            if not pipe[e[0]][e[1]]+pipe[e[0]+1][e[1]]+pipe[e[0]][e[1]+1]+pipe[e[0]+1][e[1]+1]:
+                if (e[0]+1, e[1]+1) == (N-1, N-1):
+                    cnt +=1
+                elif e[0]+1<N and e[1]+1<N:
+                    dfs((e[0]+1, e[1]+1), 0)
+        if e[1]+1<N:
+            if not pipe[e[0]][e[1]] + pipe[e[0]][e[1]+1]:
+                if (e[0], e[1]+1) == (N-1, N-1):
+                    cnt +=1
+                elif e[1]+1 < N-1:
+                    dfs((e[0], e[1]+1), 1)
+    else:
+        if e[0]+1 < N and e[1]+1<N:
+            if not pipe[e[0]][e[1]]+pipe[e[0]+1][e[1]]+pipe[e[0]][e[1]+1]+pipe[e[0]+1][e[1]+1]:
+                if (e[0]+1, e[1]+1) == (N-1, N-1):
+                    cnt +=1
+                elif e[0]+1<N and e[1]+1<N:
+                    dfs((e[0]+1, e[1]+1), 0)
+        if e[0]+1 <N:
+            if not pipe[e[0]][e[1]] + pipe[e[0]+1][e[1]]:
+                if (e[0]+1, e[1]) == (N-1, N-1):
+                    cnt +=1
+                elif e[0]+1 < N-1:
+                    dfs((e[0]+1, e[1]), 2)
 
-pipe_dp(start, end, direction)
+dfs(end, direction)
 print(cnt)
-
