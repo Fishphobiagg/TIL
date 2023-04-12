c4ca436a6f1ea10168f67c38044f362cb865c7ad
Fishphobiagg <dlauddn6211@icloud.com>
2023-04-12 17:37:49 +0900

    거짓말 풀이

--- ALG/백준/gold/1043.py
+++ ALG/백준/gold/1043.py
@@ -1,9 +1,36 @@
 # 구라
-
+'''
+파티에 온 사람들은 모두 visited 처리
+만약 다음 파티에 해당 사람들이 있을 경우 멈추고 떠들고 다닌 파티 말하기
+'''
 N, M = map(int, input().split())
-trues = list(map(int,input().split()))
+trues = list(map(int,input().split()))[1:]
 person = [[] for _ in range(N+1)]
-
-if trues == [0]:
+party = [list(map(int,input().split()))[1:] for _ in range(M)]
+if not trues:
     print(M)
     exit()
+
+for k in party:
+    for a in range(len(k)): # 기준 k, 붙일 애
+        for j in range(len(k)):
+            if j == a:
+                continue
+            person[k[a]].append(k[j])
+
+def dfs(x):
+    for i in person[x]:
+        if i not in trues:
+            trues.append(i)
+            dfs(i)
+first_true = trues[:]
+for i in first_true:
+    dfs(i)
+ans = M
+for i in party:
+    for k in trues:
+        if k in i:
+            ans-=1
+            break
+
+print(ans)
